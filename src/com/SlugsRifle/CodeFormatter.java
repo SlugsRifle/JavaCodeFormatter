db94f21005930e70d2ef4a25f719a95864d928ff
dksgusals99 <dksgusals99@gmail.com>
2020-03-14 12:01:35 +0900

    .

--- src/com/SlugsRifle/CodeFormatter.java
+++ src/com/SlugsRifle/CodeFormatter.java
@@ -1,18 +1,23 @@
 package com.SlugsRifle;
 
-public class CodeFormatter                  {
-	
-	String str                              ;
-	
-	public CodeFormatter()                  {
-		this("")                            ;}
-	
-	public CodeFormatter(String str)        {
-		this.str = str                      ;}
-	
-	public CodeFormatter set(String str)    {
-		this.str = str                      ;
-		return this                         ;}
-		
-	public CodeFormatter run()              {
-		return this                         ;}}
+public class CodeFormatter {
+
+    String str;
+
+    public CodeFormatter() {
+        this("");
+    }
+
+    public CodeFormatter(String str) {
+        this.str = str;
+    }
+
+    public CodeFormatter set(String str) {
+        this.str = str;
+        return this;
+    }
+
+    public CodeFormatter run() {
+        return this;
+    }
+}
--- src/com/SlugsRifle/Main.java
+++ src/com/SlugsRifle/Main.java
@@ -2,8 +2,10 @@ package com.SlugsRifle;
 
 import java.util.Scanner;
 
-public class Main                              {
+public class Main {
 
-	public static void main(String[] args)     {
-		Scanner sc = new Scanner(System.in)    ;
-		sc.close()                             ;}}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
